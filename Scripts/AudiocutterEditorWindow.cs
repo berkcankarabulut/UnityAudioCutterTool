7945497a5f7ccd80e0064f62545b72de76e89f77
berkcankarabulut <46574515+berkcankarabulut@users.noreply.github.com>
2025-04-04 22:57:24 +0300

    fix: Meta added. Unity_Editor added.

--- Scripts/AudiocutterEditorWindow.cs
+++ Scripts/AudiocutterEditorWindow.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
 using System.IO;
@@ -369,3 +370,4 @@ public class AudioCutterEditorWindow : EditorWindow
         StopPreview();
     }
 }
+#endif
